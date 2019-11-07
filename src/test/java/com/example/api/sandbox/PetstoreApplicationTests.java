fd8d3f8663acf0159d012078f16b0ffb3f1d749f
Jon <jonbuck16@gmail.com>
2019-11-07 19:40:00 +0000

    Added project files

---
+++ src/test/java/com/example/api/sandbox/PetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
